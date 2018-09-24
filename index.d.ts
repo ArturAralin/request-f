3f5cfe329e93876442f455ca43f7a410c8aa2176
Artur <artur.aralin97@gmail.com>
2018-09-24 13:04:57 +0300

    feat(ts declarations): update ts declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,42 @@
 import {
   RequestAPI,
-  Request,
   RequiredUriUrl,
   CoreOptions,
+  RequestAsJSON,
 } from 'request';
 import {
   Future,
 } from 'fluture';
+import {
+  Left,
+  Right,
+} from 'sanctuary';
 
 declare module "request-f" {
   type Options = RequiredUriUrl & CoreOptions;
-    export function get(uri: string): Future<any, Request>;
-    export function get(o: Options): Future<any, Request>;
+    export function get(uri: string): Future<Left<any>, Right<Request>>;
+    export function get(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function post(uri: string): Future<any, Request>;
-    export function post(o: Options): Future<any, Request>;
+    export function post(uri: string): Future<Left<any>, Right<Request>>;
+    export function post(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function head(uri: string): Future<any, Request>;
-    export function head(o: Options): Future<any, Request>;
+    export function head(uri: string): Future<Left<any>, Right<Request>>;
+    export function head(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function options(uri: string): Future<any, Request>;
-    export function options(o: Options): Future<any, Request>;
+    export function options(uri: string): Future<Left<any>, Right<Request>>;
+    export function options(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function put(uri: string): Future<any, Request>;
-    export function put(o: Options): Future<any, Request>;
+    export function put(uri: string): Future<Left<any>, Right<Request>>;
+    export function put(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function patch(uri: string): Future<any, Request>;
-    export function patch(o: Options): Future<any, Request>;
+    export function patch(uri: string): Future<Left<any>, Right<Request>>;
+    export function patch(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function del(uri: string): Future<any, Request>;
-    export function del(o: Options): Future<any, Request>;
+    export function del(uri: string): Future<Left<any>, Right<Request>>;
+    export function del(o: Options): Future<Left<any>, Right<Request>>;
 
-    export function delete(uri: string): Future<any, Request>;
-    export function delete(o: Options): Future<any, Request>;
+    export function delete(uri: string): Future<Left<any>, Right<Request>>;
+    export function delete(o: Options): Future<Left<any>, Right<Request>>;
 
   export const requestOrig: RequestAPI;
 }
